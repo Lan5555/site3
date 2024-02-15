bccee9ac336b46283dfef16e3ac73c474ef290cc
Lan5555 <145215080+Lan5555@users.noreply.github.com>
2024-02-16 00:02:03 +0100

    main.js

--- strike/main.js
+++ strike/main.js
@@ -144,7 +144,7 @@ window.onload = function() {
       img.src = asset.src;
     } else if (asset.type === 'audio') {
       var audio = new Audio();
-      audio.onloadeddata = assetLoaded;
+      audio.oncanplaythrough = assetLoaded;
       audio.onerror = function() { handleLoadError(asset); };
       audio.src = asset.src;
     }
